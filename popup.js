e6c9beb3cfb18e4e8a89ea4f494a5da1a2b799d1
akshayveer <akshayveer096@gmail.com>
2017-07-08 21:00:04 +0530

    Add execute search request method and error method of load client

--- popup.js
+++ popup.js
@@ -4,14 +4,36 @@ function start() {
   gapi.client.init({
     'apiKey': 'AIzaSyD98bluzT_oxioGc1iLm_2_O242U2f7cvU',
     'discoveryDocs': ['https://www.googleapis.com/discovery/v1/apis/youtube/v3/rest']
-  }).then(function () {
+  }).then(function (response) {
     gapi_state = 1;
     console.log('gapi initialized');
+  }, function (error) {
+    console.log(error);
   });
 };
 
 function loadClient() {
   gapi.load('client', start);
+}
+
+function executeRequest(search_query) {
+  gapi.client.request({
+    path: '/youtube/v3/search',
+    params: {
+      'maxResults': '25',
+       'part': 'snippet',
+       'q': search_query,
+       'type': ''
+    }
+  }).then(function (response) {
+    console.log(response);
+  }, function (reason) {
+    console.error(reason);
+  });
+}
+
+function search(search_query) {
+  executeRequest(search_query);
 }
 
 $('#search_term').keypress(function (e) {
